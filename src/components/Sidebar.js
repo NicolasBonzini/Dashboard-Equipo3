850a8805c429dc45ee3dbc9f1dec552cc1345ef3
NicolasBonzini <nicolas.bonzini@cencosud.com>
2022-10-06 14:16:10 -0300

    Sidebar en lineas generales completo

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -14,41 +14,47 @@ import Avatar from "./Avatar";
 import Brand from "./Brand";
 
 function Sidebar() {
-  return (
-    <div className="sidebar dark">
-      {/* Top Navbar */}
-      <nav className="top-navbar">
-        <Brand />
-        <nav className="nav-list">
-          <ul>
-            <NavLink to="/">
-              <li>
-                <FontAwesomeIcon icon={faHouse} className="icon" />
-                Inicio
-              </li>
-            </NavLink>
-            <NavLink to="/products">
-              <li>
-                <FontAwesomeIcon icon={faBoxOpen} className="icon" />
-                Productos
-              </li>
-            </NavLink>
-            <NavLink to="/stores">
-              <li>
-                <FontAwesomeIcon icon={faStoreAlt} className="icon" />
-                Tiendas
-              </li>
-            </NavLink>
-          </ul>
-        </nav>
-      </nav>
-      {/* Bottom Navbar */}
-      <nav className="bottom-navbar">
 
-        <NavLink to="/profile">
-          <Avatar />
-        </NavLink>
-      </nav>
+  return (
+    <div className="sidebar">
+      {/* Sidebar */}
+      <div className="left-sidebar">
+        {/* Top Navbar */}
+        <nav className="top-navbar">
+          <Brand />
+          <nav className="nav-list">
+            <ul>
+              <NavLink to="/">
+                <li>
+                  <FontAwesomeIcon icon={faHouse} className="icon" />
+                  Inicio
+                </li>
+              </NavLink>
+              <NavLink to="/products">
+                <li>
+                  <FontAwesomeIcon icon={faBoxOpen} className="icon" />
+                  Productos
+                </li>
+              </NavLink>
+              <NavLink to="/stores">
+                <li>
+                  <FontAwesomeIcon icon={faStoreAlt} className="icon" />
+                  Tiendas
+                </li>
+              </NavLink>
+            </ul>
+          </nav>
+        </nav>
+        {/* Bottom Navbar */}
+        <nav className="bottom-navbar">
+          <NavLink to="/profile">
+            <Avatar />
+          </NavLink>
+        </nav>
+      </div>
+      {/* Space to click in and close sidebar */}
+      <div 
+        className="right-sidebar" ></div>
     </div>
   );
 }
