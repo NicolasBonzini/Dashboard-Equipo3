9f7b95f260b2dc4ca4c0016d6a96df0d5e02edfb
NachoBarb <ignaciobarbisancosta@gmail.com>
2022-10-06 12:37:46 -0300

    Estilo US4

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../../components/Header'
 import HomeContent from '../../components/HomeContent'
+import CardStartPage from '../../components/CardStartPage'
 
 function Home() {
   return (
     <div className='home'>
       <Header />
       <HomeContent />
+      <CardStartPage/>
     </div>
   )
 }
