ba33cfae467e5b141ae81f568dfb30787e8ec1f5
santiago_del_rivero <sdelrive97@gmail.com>
2022-10-06 16:08:17 -0300

    arreglando colores de fondo

--- src/pages/products/productView/ProductView.js
+++ src/pages/products/productView/ProductView.js
@@ -25,7 +25,7 @@ function ProductView() {
         </div>
         <Button text='Eliminar' personalClass='btnDelete'/>
       </Header>
-        <div className='cont'>
+  <div className='cont'>
 
     <div className='productView'>
       <EditProduct/>
