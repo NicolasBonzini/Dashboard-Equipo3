33beabec6df25884b269849d0c4ec8a3f7889fab
NachoCencosud <pablo.barbisan@cencosud.com>
2022-10-09 01:27:11 -0300

    Finalizando US 6

--- src/components/CardStartPage.js
+++ src/components/CardStartPage.js
@@ -1,24 +1,37 @@
 import React from 'react'
-import "../assets/styles/cardStartPage.css";
+import "../assets/styles/CardStartPage.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
     faHouse,
 } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from 'react-router-dom';
 
-const cardStartPage = () => {
+const CardStartPage = ({listProd}) => {
 
+    const navigate = useNavigate();
+    
+
+    console.log(listProd);
+
+    const handleListProducts = () => {
+        navigate('/products');
+    }
+
+    const handleAddProducts = () => {
+        navigate('/product/21');
+    }
 
     return (
         <div className='card-content'>
             <div className='card-logo'><FontAwesomeIcon icon={faHouse} /></div>
-            <p className='card-number'>123</p>
-            <p className='card-text'>Texto</p>
+            <p className='card-number'>{listProd}</p>
+            <p className='card-text'>Productos</p>
             <div className='card-button-content'>
-                <button className='card-button'>Ver Listado</button>
-                <button className='card-button'>Agregar Producto</button>
+                <button className='card-button' onClick={handleListProducts}>Ver Listado</button>
+                <button className='card-button' onClick={handleAddProducts}>Agregar Producto</button>
             </div>
         </div>
     )
 }
 
-export default cardStartPage
+export default CardStartPage
