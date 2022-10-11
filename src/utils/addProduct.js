6e86457ddd8de240d5c6bf8c103fbc336e1648f5
NachoCencosud <pablo.barbisan@cencosud.com>
2022-10-10 21:09:45 -0300

    fix problems

--- src/utils/addProduct.js
+++ src/utils/addProduct.js
@@ -1,17 +1,30 @@
 
 
-const addProduct = (form) => {
+// const addProduct = (form) => {
 
-    fetch('http://localhost:5000/api/product', {
+//     fetch('http://localhost:5000/api/product', {
+//     method: 'POST',
+//     headers: {
+//         'Content-type': 'application/json'
+//     }, body: JSON.stringify(form)
+// })
+//         .then(resp => resp.json())
+//         .then(data => { return JSON.stringify(data) })
+//         .catch(error => { return error })
+
+// }
+
+const addProduct = async (form) => {
+    const res = await fetch('http://localhost:5000/api/product', {
         method: 'POST',
         headers: {
             'Content-type': 'application/json'
         }, body: JSON.stringify(form)
     })
-        .then(resp => resp.json())
-        .then(data => { return JSON.stringify(data) })
-        .catch(error => { return error })
 
+    return (
+        res
+    )
 }
 
 export default addProduct;
