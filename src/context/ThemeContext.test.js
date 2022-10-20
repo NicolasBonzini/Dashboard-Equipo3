4491344f2d9f7d2a6273e2b55b929c027b91fed2
NicolasBonzini <nicolas.bonzini@cencosud.com>
2022-10-19 22:04:34 -0300

    Test of Theme Context

---
+++ src/context/ThemeContext.test.js
@@ -0,0 +1,28 @@
+import { getByRole, logRoles, render, screen } from "@testing-library/react";
+import { ThemeProvider } from "./ThemeContext";
+import userEvent from "@testing-library/user-event";
+import LinksNavegationSideBar from "../components/LinksNavegationSideBar/LinksNavegationSideBar";
+import { MemoryRouter } from "react-router-dom";
+
+describe("ThemeContext", () => {
+  test("Change to dark theme", async () => {
+    const user = userEvent.setup();
+
+    const { container } = render(
+      <MemoryRouter>
+        <ThemeProvider>
+          <LinksNavegationSideBar />
+        </ThemeProvider>
+      </MemoryRouter>
+    );
+
+    logRoles(container);
+    const themeDefault = screen.getByTestId("");
+    const label = screen.getByRole("button", { name: "★ ★" });
+
+    expect(themeDefault.dataset.testid).toMatch("");
+
+    await user.click(label);
+    expect(themeDefault.dataset.testid).toMatch(/dark/i);
+  });
+});
