808ed6f78a1878f24a001e85e1a76e0cc029b5b8
santiago_del_rivero <sdelrive97@gmail.com>
2022-10-19 23:26:01 -0300

    push para merge

--- src/components/ProductCard/ProductCard.test.js
+++ src/components/ProductCard/ProductCard.test.js
@@ -6,32 +6,28 @@ import {
   logRoles,
 } from "@testing-library/react";
 import { useParams, BrowserRouter, MemoryRouter } from "react-router-dom";
-import ProductCard from '../ProductCard/ProductCard'
-
+import ProductCard from "../ProductCard/ProductCard";
 
 describe("This component must render props", () => {
+  beforeEach(() => {
+    act(() => {
+      render(
+        <MemoryRouter>
+          <ProductCard
+            key={1}
+            price={549}
+            id={1}
+            title={"iPhone 9"}
+            image={"https://dummyjson.com/image/i/products/1/1.jpg"}
+          />
+        </MemoryRouter>
+      );
+    });
+  });
 
-    beforeEach(() => {
-    
-        act(() => {
-          render(
-            <MemoryRouter>
-              <ProductCard  key={1}
-                    price={549}
-                    id={1}
-                    title={"iPhone 9"}
-                    image={"https://dummyjson.com/image/i/products/1/1.jpg"} />
-            </MemoryRouter>
-          );
-        });
-      });
-
-    it('Props.price must be correctly rendered', ()=>{
-       const item = screen.getByText(/549/i);
-       logRoles(item);
-        expect(item).toHaveTextContent('$549.00');
-
-    })
-
-
+  it("Props.price must be correctly rendered", () => {
+    const item = screen.getByText(/549/i);
+    logRoles(item);
+    expect(item).toHaveTextContent("$549.00");
+  });
 });
